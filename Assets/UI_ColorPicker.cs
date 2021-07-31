f585cc667f878ae8cc52f699bb42f48b3d3303de
Muharrem Sait ÇOKTAŞ <coktasmuharremsait@gmail.com>
2021-07-31 15:00:47 +0300

    First Commit

---
+++ Assets/UI_ColorPicker.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UI_ColorPicker : MonoBehaviour
+{
+	//The target object: GameObject whose Material we want to change.
+	public GameObject target;
+	private Material targetMaterial;
+	private GraphicRaycaster rc;
+	private PointerEventData pt;
+	private EventSystem eventSystem;
+
+	void Start(){
+		//Obtain the target object's material
+		if(target != null){
+			targetMaterial = target.GetComponent<Renderer>().material;
+		} 
+		//Obtain the Canvas Raycaster and EventSystem Components
+		rc = GetComponent<GraphicRaycaster>();
+		eventSystem = GetComponent<EventSystem>();
+	}
+
+	void Update(){
+		/*responsible for registering a "hit" and keeping track of all the objects hit by the
+		Graphic Raycaster in our Canvas.These Objects are the swatches we created earlier.
+		For every swatch our Raycaster hits, we only want to pay attention to the color of
+		its image component.*/
+		pt = new PointerEventData(eventSystem);
+		pt.position = Input.mousePosition;
+
+		List<RaycastResult> results = new List<RaycastResult>();
+		rc.Raycast(pt, results);
+
+		foreach(RaycastResult swatch in results){
+			if(swatch.gameObject.GetComponent<Image>().color != null){
+				ChangeColor(swatch.gameObject.GetComponent<Image>().color);
+			}
+		}
+	}
+
+	void ChangeColor(Color c){
+		targetMaterial.color = c;
+	}
+
+}
